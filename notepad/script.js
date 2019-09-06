3318fe6816e2de79ac70f42ad504ee634ee92a20
Grathium <grathiumsoftwears@gmail.com>
2019-09-06 21:37:32 +1000

    Update script.js

--- notepad/script.js
+++ notepad/script.js
@@ -1,11 +1,11 @@
 function download(){
 	var link = document.createElement('a');
-	var str = document.getElementById("text").value;
+	var str = document.getElementById("mainInput").value;
 	str = str.split("\u000A").join("\u000D\u000A");
 	bl = new Blob([str]);
 	link.href = URL.createObjectURL(bl);
-	if (document.getElementById("text").name!=""){
-		link.download = document.getElementById("text").name;
+	if (document.getElementById("mainInput").name!=""){
+		link.download = document.getElementById("mainInput").name;
 	} else {
 		link.download = "text.txt";
 	}
@@ -14,11 +14,11 @@ function download(){
 }
 function readfile(filelist){
 	var text = filelist[0];
-	document.getElementById("text").name=text.name;
+	document.getElementById("mainInput").name=text.name;
 	console.dir(text);
 	var reader = new FileReader();
 	reader.onload = function(e) {
-    document.getElementById("text").value = e.target.result;
+    document.getElementById("mainInput").value = e.target.result;
   };
   reader.readAsText(text);
 }
