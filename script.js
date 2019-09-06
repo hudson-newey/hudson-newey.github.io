ee37fe26ed1e924ffb0de765f9b9e2423742352c
Grathium <grathiumsoftwears@gmail.com>
2019-09-06 20:54:44 +1000

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+function navigate(e) {
+ var key=e.keyCode || e.which;
+  if (key==13) {
+    if (document.getElementById('search-bar').value.includes("://"))
+      document.location.href = document.getElementById('search-bar').value;
+    else
+      document.location.href = "https://www.google.com/search?q=" + document.getElementById('search-bar').value;
+  }
+
+  // hide search bar
+  if (key==27) {
+    document.getElementById("search-bar").style.display = "none";
+  }
+}
+
+function showSearch() {
+  document.getElementById('search-bar').style.display = "inline";
+  document.getElementById('search-bar').focus();
+}
