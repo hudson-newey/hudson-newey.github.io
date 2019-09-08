22994582ea6a024c1a88c2e93f8bf3866fa48f06
Grathium <grathiumsoftwears@gmail.com>
2019-09-08 21:05:08 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// Initilization
+var addingPrograms = false;
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function GetSubstringIndex(str, substring, n) {
+    var times = 0, index = null;
+
+    while (times < n && index !== -1) {
+        index = str.indexOf(substring, index+1);
+        times++;
+    }
+
+    return index;
+}
+
+
+
+
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -11,11 +47,19 @@ function closeNav() {
 SB = document.getElementById('search-bar')
 function navigate(e) {
  var key=e.keyCode || e.which;
-  if (key==13 && !(SB.value == "" || SB.value == null)) {
+  if ((key==13 && !(SB.value == "" || SB.value == null)) && addingPrograms == false) {
       if (SB.value.includes("://"))
         document.location.href = SB.value;
       else
         document.location.href = "https://duckduckgo.com/?q=" + SB.value + "&kj=#21252b&kx=#00afff&k7=#282c34&k8=#abb2bf&k9=#cbd2df&kaa=#abb2bf";
+  }
+
+  // adding adding programs
+  if ((key==13 && !(SB.value == "" || SB.value == null)) && addingPrograms == true) {
+    var temps = SB.value;
+    SB.style.display = "none";
+    SB.value = "";
+    createProgram(temps, false);
   }
 
   // hide search bar
@@ -27,9 +71,33 @@ function navigate(e) {
 
 function showSearch(e) {
   var key=e.keyCode || e.which;
-  if (key!=27 && ((key > 64 && key < 111) || (key > 186 && key < 223)))
+  if (key!=27 && ((key > 64 && key < 111) || (key > 186 && key < 223) || key == "undefined"))
     SB.style.display = "inline";
     SB.focus();
+}
+
+// when the user clicks on the plus/add button
+function addProgram() {
+  SB.style.display = "inline";
+  SB.focus();
+  addingPrograms = true;
+  closeNav();
+}
+
+// add the new program to the sidebar
+function createProgram(program, install) {
+  var newProgram = document.createElement("a");
+  newProgram.innerHTML = program;
+  newProgram.href = program + "/index.html";
+  newProgram.className = "menuitem";
+  document.getElementById("mySidenav").appendChild(newProgram);
+
+  if (install == false) { // if it is a new program
+    // create cookie
+    var d = new Date();
+    // individual programs are seperated by a exclemation point
+    document.cookie = 'InstalledPrograms=' + getCookie("InstalledPrograms") + '' + program + '!; expires=' + days[d.getDay()] + ', ' + d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear() + ' 20:47:11 UTC; path=/'
+  }
 }
 
 function infinateScroll() {
@@ -39,8 +107,28 @@ function infinateScroll() {
   }
 }
 
-function start() {
+function loop() {
   infinateScroll();
-  setTimeout(start, 100);
+  setTimeout(loop, 100);
 }
-start();
+
+
+// add previously installed programs
+var programList = getCookie("InstalledPrograms");
+var programCount = (programList.match(/!/g) || []).length;
+
+var pointer = 0;
+var oldPointer = 0;
+for (var i = 0; i < programCount; i++) {
+  oldPointer = pointer;
+  pointer = GetSubstringIndex(programList, "!", i + 1);
+  var loadPrograms = programList.substring(oldPointer, pointer);
+  loadPrograms = loadPrograms.replace('!','');
+  createProgram(loadPrograms, true);
+}
+
+
+
+
+
+loop();
