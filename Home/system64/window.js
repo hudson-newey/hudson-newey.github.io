388d3bfbb71480a9989d88ffcad668fc7cd51d4c
Grathium <grathiumsoftwears@gmail.com>
2019-09-15 12:50:09 +1000

    Add files via upload

---
+++ Home/system64/window.js
@@ -0,0 +1,50 @@
+const { app, BrowserWindow, globalShortcut } = require('electron')
+var urls = [
+    "http://localhost:8080"
+]
+
+
+const createWindow = () =>{
+    win = new BrowserWindow({
+        center: true,
+        fullscreen: true,
+        alwaysOnTop: true,
+        title: 'Grathium UI',
+        webPreferences:{
+          plugins: true,
+          nodeIntegration: false,
+          show: false
+        }
+    });
+
+    win.removeMenu();
+    win.maximize();
+    console.log(urls[0]);
+
+    win.loadURL(urls[0]);
+    win.once('ready-to-show',()=>{
+        win.show()
+    });
+
+    win.on('closed',()=>{
+        win = null;
+    });
+}
+
+function createGlobalShortcut() {
+    const homekey = "CommandOrControl+H";
+    globalShortcut.register(homekey, () => {
+        win.loadURL(urls[0]);
+    });
+
+    console.log(
+        "Homepage Set to: " + homekey,
+        globalShortcut.isRegistered(homekey)
+    );
+}
+
+let win = null;
+app.on("ready", () => {
+    createWindow();
+    createGlobalShortcut();
+});
